cc9351adf7e35cc7564c396576e50cc394e2b546
Carlos <carlos.contralogo@gmail.com>
2018-07-12 12:36:37 -0500

    fix styles resultados

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDRGQEH8h7LFNCXYvp63RZC2BLE08wf0eQ',
+    authDomain: 'imago-happyland.firebaseapp.com',
+    databaseURL: 'https://imago-happyland.firebaseio.com',
+    projectId: 'imago-happyland',
+    storageBucket: 'imago-happyland.appspot.com',
+    messagingSenderId: '313895401905',
+    timestampsInSnapshots: true
+  }
 };
