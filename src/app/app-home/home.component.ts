727d26f63be11bc77241dd216a1d7e86693596bd
Enrique Juan de Dios <juandedioz@gmail.com>
2018-07-12 17:25:09 -0500

    Added Model Checkboxes

--- src/app/app-home/home.component.ts
+++ src/app/app-home/home.component.ts
@@ -20,7 +20,69 @@ export class HomeComponent implements OnInit {
       canje: '',
       recepcion: '',
       experiencia: '',
-      unidades: ''
+      unidades: '',
+      unidadesnegocio: [
+        {
+          title: '*Happy Birthday (área de cumpleaños)',
+          value: 1,
+          checked: false
+        },
+        {
+          title: '*Happy Kids',
+          value: 2,
+          checked: false
+        },
+        {
+          title: '*Happy Divas',
+          value: 3,
+          checked: false
+        }
+      ],
+      incidencias: [
+        {
+          title: 'Paredes manchadas',
+          value: 1,
+          checked: false
+        },
+        {
+          title: 'Pisos sucios o con basura.',
+          value: 2,
+          checked: false
+        },
+        {
+          title: 'Máquinas sucias/en mal estado',
+          value: 3,
+          checked: false
+        },
+        {
+          title: 'Mala iluminación.',
+          value: 3,
+          checked: false
+        },
+        {
+          title:
+            'Canje no está surtido con productos de diferente valor en tickets.',
+          value: 3,
+          checked: false
+        },
+        {
+          title:
+            'Happy Sweet: Sin precios puestos o no está surtido de productos como: pop corn, churros, hotdogs).',
+          value: 3,
+          checked: false
+        },
+        {
+          title: 'Local no tiene señaletica de seguridad.',
+          value: 3,
+          checked: false
+        },
+        {
+          title:
+            'Otros (desperdicios, cajas, bolsas, equipos de limpieza, etc.arrinconados por el local)',
+          value: 3,
+          checked: false
+        }
+      ]
     });
   }
   images = [{ value: 'word1.jpg' }, { value: 'word2.jpg' }, { value: '' }];
@@ -35,7 +97,7 @@ export class HomeComponent implements OnInit {
   }
   onSubmit(form: any) {
     console.log('test', form.value);
-    this.dataService.setDatas(form.value);
+    // this.dataService.setDatas(form.value);
   }
-  onFileChange(event: any) { }
+  onFileChange(event: any) {}
 }
