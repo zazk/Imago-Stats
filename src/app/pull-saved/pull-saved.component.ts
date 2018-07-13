1bd40a243a7a87bbdceaca27a380fb2380eb7a09
Carlos <carlos.contralogo@gmail.com>
2018-07-13 12:48:43 -0500

    added pull saved component

---
+++ src/app/pull-saved/pull-saved.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pull-saved',
+  templateUrl: './pull-saved.component.html',
+  styleUrls: ['./pull-saved.component.css']
+})
+export class PullSavedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
