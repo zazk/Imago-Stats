6c84524aba77d02e57d6577ccf4f70f3b7aa6ecd
Carlos <carlos.contralogo@gmail.com>
2018-07-17 10:29:51 -0500

    added upload sucursales to sucursales

--- src/app/sucursales/sucursales.component.ts
+++ src/app/sucursales/sucursales.component.ts
@@ -11,13 +11,24 @@ import { AngularFireStorage } from 'angularfire2/storage';
   styleUrls: ['./sucursales.component.css']
 })
 export class SucursalesComponent implements OnInit {
-
+  form: FormGroup;
   sucursales: any[];
   constructor(
     public sucursalesService: SucursalesService,
     private fb: FormBuilder,
     private router: Router,
-    private afStorage: AngularFireStorage) { }
+    private afStorage: AngularFireStorage) {
+    this.form = fb.group({
+      nombre: '',
+    });
+  }
+
+  buildParam(param: any[], arr: any[]) {
+    return param.map((checked, i) => ({
+      id: arr[i].value,
+      selected: checked
+    }));
+  }
 
   ngOnInit() {
     this.sucursalesService.getSucursales().subscribe(sucursales => {
@@ -25,4 +36,10 @@ export class SucursalesComponent implements OnInit {
     });
   }
 
+  onSubmit(form: any) {
+    const obj = Object.assign({}, form.value);
+    this.sucursalesService.setSucursales(obj).subscribe(r => {
+      console.log('R', r);
+    });
+  }
 }
