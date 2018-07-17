04e562b5b7a14a0488a3a7abf6542ff96292d3f8
Enrique Juan de Dios <juandedioz@gmail.com>
2018-07-17 16:28:34 -0500

    Added Promedio

--- src/app/resultados-sucursales/resultados-sucursales.component.ts
+++ src/app/resultados-sucursales/resultados-sucursales.component.ts
@@ -7,14 +7,21 @@ import { DataService } from '../services/data.service';
   styleUrls: ['./resultados-sucursales.component.css']
 })
 export class ResultadosSucursalesComponent implements OnInit {
-
+  sumas: any = { c_calidadAtencion: 0 };
   datas: any[];
-  constructor(public dataService: DataService) { }
+  constructor(public dataService: DataService) {}
 
   ngOnInit() {
     this.dataService.getDatas().subscribe(datas => {
       this.datas = datas;
+      this.datas.forEach(data => {
+        this.sumas.c_calidadAtencion += parseInt(data.c_calidadAtencion, 10);
+        console.log('CALIDAD ATENCION', data.c_calidadAtencion);
+      });
+      this.sumas.c_calidadAtencion =
+        this.sumas.c_calidadAtencion / this.datas.length;
+      console.log(datas);
+      console.log('TOTAL SUMAS', this.sumas);
     });
   }
-
 }
